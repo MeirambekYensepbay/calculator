c8f1c5229950faad83937e21a81cf35f1b7f606f
Miko <32616763+MeirambekYensepbay@users.noreply.github.com>
2017-11-06 19:09:49 +0600

    Initial commit

---
+++ methods.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class methods {
+	
+	double plus(double a, double b){
+		a =  a + b;
+		return a;
+	}
+	double minus(double a, double b){
+		return a-b;
+	}
+	double divide(double a, double b){
+		return a/b;
+	}
+	double multiply(double a, double b){
+		return a*b;
+	}
+	double percent(double a, double b){
+		return a*b/100;
+	}
+}
